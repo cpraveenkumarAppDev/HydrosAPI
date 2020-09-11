c801064ccad9dbf419b78633070f8d2648ef7664
Ian Sutherland <isutherland@azwater.gov>
2020-09-11 12:14:08 -0700

    docushare service

---
+++ HydrosApi/Models/Docushare/SOCDOC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdwrApi.Models.Docushare
+{
+    public class SOCDOC
+    {
+        public string Handle { get; set; }
+        public string DocType { get; set; }
+        public string FileName { get; set; }
+        public string FileUrl { get; set; }
+        public string ObjSummary { get; set; }
+        public string FileIdentifier { get; set; }
+    }
+}
---
+++ HydrosApi/Services/DocushareService.cs
@@ -0,0 +1,37 @@
+﻿using AdwrApi.Models.Docushare;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AdwrApi.Services
+{
+    public class DocushareService
+    {
+        private HttpClient client;
+
+        public DocushareService()
+        {
+            this.client = new HttpClient();
+            this.client.BaseAddress = new Uri("dwrsrvc.azwater.gov/dsapi/api/");
+        }
+        public async Task<string> getSocDoc(string pcc)
+        {
+            //call DSAPI
+            var result = await this.client.GetAsync("soc/getsocdocuments?fileNumber=" + pcc);
+
+            if (result.IsSuccessStatusCode)
+            {
+                //send json object back
+                var content = await result.Content.ReadAsStringAsync();
+                var socDocs = Newtonsoft.Json.JsonConvert.DeserializeObject<List<SOCDOC>>(content);
+                var firstDoc = socDocs.FirstOrDefault();
+                return firstDoc.FileUrl;
+            }
+
+            return $"There was a non success code sent back from the DSAPI: {result.StatusCode}";
+        }
+    }
+}
