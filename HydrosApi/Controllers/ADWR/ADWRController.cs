1e4e624386f33469e327abcfd1a9afd6d1c1ab32
Jacob Nelson <jnelson@azwater.gov>
2020-10-09 07:49:01 -0700

    added db notification to windowsAuthentication

--- HydrosApi/Controllers/ADWR/ADWRController.cs
+++ HydrosApi/Controllers/ADWR/ADWRController.cs
@@ -2,6 +2,7 @@
 using System.DirectoryServices.AccountManagement;
 using System.Collections.Generic;
 using System;
+using System.Configuration;
 
 namespace HydrosApi
 {
@@ -17,59 +18,61 @@ namespace HydrosApi
             //comment test ron
             //To autmoatically login -> http://www.scip.be/index.php?Page=ArticlesNET38&Lang=EN
             var user = User.Identity.Name;
+            var environment = ConfigurationManager.ConnectionStrings["ADWRContext"].ToString().Split(';')[0].Split('=')[1] != "ADWR";
             if (user.Equals(""))
             {
                 return Unauthorized();
             }
             else
             {
-                return Ok(user);
-            }
-        }
-
-        [HttpGet]
-        [Route("adwr/windowsgroup/{groupId}")]
-        [Authorize]
-        public IHttpActionResult WindowsGroup(string groupId)
-        {
-            using (PrincipalContext ctx = new PrincipalContext(ContextType.Domain, "AZWATER0"))
-            {
-                // find a user
-                UserPrincipal foundUsername = UserPrincipal.FindByIdentity(ctx, User.Identity.Name);
-                var unformattedGroupId = groupId.Replace("~AND~", " & "); //cannot pass & sign through url query
-                GroupPrincipal group = GroupPrincipal.FindByIdentity(ctx, unformattedGroupId);
-                GroupPrincipal appDevGroup = GroupPrincipal.FindByIdentity(ctx, "PG-APPDEV");
-                bool foundUserInGroup = foundUsername.IsMemberOf(group);
-                bool foundUserInAppDevGroup = foundUsername.IsMemberOf(appDevGroup);
-                // if found....
-                if (group != null && foundUserInGroup || foundUserInAppDevGroup)
-                {
-
-                    var groupList = new List<object>();
-                    foreach (UserPrincipal p in group.GetMembers())
-                    {
-
-                        var user = new WindowsUser
-                        {
-                            UserName = p.DisplayName,
-                            UserEmail = p.EmailAddress
-                        };
-                        groupList.Add(user);
-                    }
-                    return Ok(groupList);
-                }
-                else
-                {
-                    return BadRequest("Invalid User");
-                }
-            }
+                var validUser = new { appEnv = environment, user = user };
+                return Ok(validUser);
         }
     }
 
-    public class WindowsUser
+    [HttpGet]
+    [Route("adwr/windowsgroup/{groupId}")]
+    [Authorize]
+    public IHttpActionResult WindowsGroup(string groupId)
     {
-        public string UserName { get; set; }
-        public string UserEmail { get; set; }
+        using (PrincipalContext ctx = new PrincipalContext(ContextType.Domain, "AZWATER0"))
+        {
+            // find a user
+            UserPrincipal foundUsername = UserPrincipal.FindByIdentity(ctx, User.Identity.Name);
+            var unformattedGroupId = groupId.Replace("~AND~", " & "); //cannot pass & sign through url query
+            GroupPrincipal group = GroupPrincipal.FindByIdentity(ctx, unformattedGroupId);
+            GroupPrincipal appDevGroup = GroupPrincipal.FindByIdentity(ctx, "PG-APPDEV");
+            bool foundUserInGroup = foundUsername.IsMemberOf(group);
+            bool foundUserInAppDevGroup = foundUsername.IsMemberOf(appDevGroup);
+            // if found....
+            if (group != null && foundUserInGroup || foundUserInAppDevGroup)
+            {
+
+                var groupList = new List<object>();
+                foreach (UserPrincipal p in group.GetMembers())
+                {
+
+                    var user = new WindowsUser
+                    {
+                        UserName = p.DisplayName,
+                        UserEmail = p.EmailAddress
+                    };
+                    groupList.Add(user);
+                }
+                return Ok(groupList);
+            }
+            else
+            {
+                return BadRequest("Invalid User");
+            }
+        }
     }
+}
+
+public class WindowsUser
+{
+    public string UserName { get; set; }
+    public string UserEmail { get; set; }
+}
 
 }
