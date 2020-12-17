f85f120ad52572a9ff7b5467086708d9ff5c0db9
Ian Sutherland <isutherland@azwater.gov>
2020-12-17 11:50:24 -0700

    created new endpoint for counties and ama and basins

---
+++ HydrosApi/ViewModel/ADWR/GeoBoundaryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using HydrosApi.Models;
+using HydrosApi.Models.Permitting.AAWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HydrosApi.ViewModel.ADWR
+{
+    public class GeoBoundaryViewModel
+    {
+        public List<V_AWS_SUBBAS> SubbasinList { get; set; }
+        public List<CD_AMA_INA> AmaIna { get; set; }
+        public List<V_AWS_COUNTY_BASIN> CountyBasinList { get; set; }//TODO change this to adwr_admin schema and namespace
+
+        public GeoBoundaryViewModel()
+        {
+            this.SubbasinList = V_AWS_SUBBAS.GetAll();
+            this.AmaIna = CD_AMA_INA.GetAll();
+            this.CountyBasinList = V_AWS_COUNTY_BASIN.GetAll();
+        }
+    }
+}
