0dd12a3e599b7f0815e2ab367e501330a32fcba3
Ian Sutherland <isutherland@azwater.gov>
2021-01-19 14:45:26 -0700

    added rgr cust type code table and endpoint

--- HydrosApi/Data/OracleContext.cs
+++ HydrosApi/Data/OracleContext.cs
@@ -47,6 +47,7 @@ namespace HydrosApi.Data
         public virtual DbSet<V_AWS_CUSTOMER_LONG_NAME> V_AWS_CUSTOMER_LONG_NAME { get; set; }
         public virtual DbSet<CUSTOMER> CUSTOMER { get; set; }
         public virtual DbSet<WRF_CUST> WRF_CUST { get; set; }
+        public virtual DbSet<CD_CUST_TYPE> CD_CUST_TYPE { get; set; }
         //public virtual DbSet<EXPLANATIONS> EXPLANATIONS { get; set; }
         //public virtual DbSet<WATERSHED_FILE_REPORT> WATERSHED_FILE_REPORT { get; set; }
 
---
+++ HydrosApi/Models/ADWR/CD_CUST_TYPE.cs
@@ -0,0 +1,19 @@
+﻿using HydrosApi.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HydrosApi.Models.ADWR
+{
+    [Table("RGR.CD_CUST_TYPE")]
+    public class CD_CUST_TYPE : Repository<CD_CUST_TYPE>
+    {
+        [Key, Column("CODE"), StringLength(4)]
+        public string CODE { get; set; }
+        [Column("DESCR"), StringLength(40)]
+        public string DESCR { get; set; }
+    }
+}
