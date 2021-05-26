66c970881ae381ed936c5fc48f279e80dd6fad7b
Ian Sutherland <isutherland@azwater.gov>
2021-05-26 10:28:37 -0700

    added setter for legalAvail pcc

--- HydrosApi/Models/Permitting/AAWS/AwLegalAvailability.cs
+++ HydrosApi/Models/Permitting/AAWS/AwLegalAvailability.cs
@@ -82,6 +82,10 @@ namespace HydrosApi.Models.Permitting.AAWS
                 else
                     return WaterRightFacility.Get(f => f.Id == ProviderReceiverId).PCC;
             }
+            set
+            {
+                this.ProviderReceiverId = QueryResult.RgrRptGet(value);
+            }
         }
 
     }
