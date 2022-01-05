435d7d9696abefcb6326884eb549eb6090ee416d
Allyson Moskovits <amoskovits@azwater.gov>
2022-01-05 12:31:57 -0700

    Restore fix for original file name

--- HydrosApi/App_Start/WebApiConfig.cs
+++ HydrosApi/App_Start/WebApiConfig.cs
@@ -8,14 +8,13 @@ using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Net.Http.Headers;
-using App_Start;
-using System.Web.Http.ExceptionHandling;
+  
 
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
-            config.Services.Replace(typeof(IExceptionLogger), new Logger());
+           
 
             // Web API configuration and services
             var cors = new EnableCorsAttribute(ConfigurationManager.AppSettings["hydrosAppUrl"], "Access-Control-Allow-Headers,Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With", "GET, POST, PUT, DELETE, OPTIONS") { SupportsCredentials = true };
